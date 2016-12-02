48bc5eb49eeebd2cec56c58ec6b4edd8d76fc1c0
seanhugh <squash110@gmail.com>
2016-12-01 23:19:12 -0500

    Began Adding Frontend

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()

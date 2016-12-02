14aff3213d80ad350878dd116231ef202c998eda
seanhugh <squash110@gmail.com>
2016-12-02 18:22:14 -0500

    More autocomplete

--- static/myScript.js
+++ static/myScript.js
@@ -1,9 +1,30 @@
-$ (function() {
-    var availableTags = $('#my-data').data()['list'];
-    $( "#concertSearch" ).autocomplete({
-      source: availableTags
-    });
-  } );
+// $ (function() {
+//     var availableTags = $('#my-data').data()['list'];
+//     $( "#concertSearch" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
+
+$("#concertSearch").kendoAutoComplete({
+    dataTextField: "Name",
+    select: function(e) {
+        var dataItem = this.dataItem(e.item.index());
+        
+        //output selected dataItem
+        $("#result").html(kendo.stringify(dataItem));       
+    },
+    dataSource: {
+        data: [
+            { Name : "GB1", value: 1 },
+            { Name : "GB2", value: 2 },
+            { Name : "GB3", value: 3 },
+            { Name : "GB4", value: 4 }
+        ]
+    }
+});
+
+
+
 
 console.log($('#my-data').data());
 var data = $('#my-data').data()['list'];
